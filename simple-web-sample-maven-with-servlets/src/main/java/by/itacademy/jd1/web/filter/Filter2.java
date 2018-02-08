6cce2efda8153ecd59bcb637763e367205e497e7
Fhntv <said-kirill@rambler.ru>
2018-02-08 21:25:07 +0300

    no message

---
+++ simple-web-sample-maven-with-servlets/src/main/java/by/itacademy/jd1/web/filter/Filter2.java
@@ -0,0 +1,32 @@
+package by.itacademy.jd1.web.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Filter2 implements Filter {
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>");
+		writer.println("Filter2 was here");
+		writer.println("</h1>");
+
+		chain.doFilter(request, response);
+
+	}
+
+	public void destroy() {
+	}
+
+}
